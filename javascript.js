8704b25168f46f0274321d5e758859b5a5664c21
aran0047 <145908176+aran0047@users.noreply.github.com>
2023-10-17 21:37:42 +1100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,16 +1,20 @@
-const dropdown = document.getElementById('scatterplot-dropdown');
-const container = document.getElementById('scatterplot-container');
+var specMap1 = "./index_line.json";
+var specMap2 = "./map1.json";
+var specVis1 = "./scatter_index_talent.json";
+var specVis2 = "./scatter_index_infrastructure.json";
+var specVis3 = "./scatter_index_oe.json";
+var specVis4 = "./scatter_index_research.json";
+var specVis5 = "./scatter_index_development.json";
+var specVis6 = "./scatter_index_commercial.json";
+var specVis7 = "./scatter_index_gov.json";
 
-async function displayScatterplot(selectedValue) {
-    let selectedSpec, explanation;
 
-    try {
-        const response = await fetch('scatter_index_${selectedValue}.json');
-        if (!response.ok) {
-            throw new Error('Error');
-        }
-
-        selectedSpec = await response.json();
-        explanation p 
-    }
-}
+vegaEmbed('#line_all', specMap1, {}).then(result => {}).catch(console.error);
+vegaEmbed('#map', specMap2, {}).then(result => {}).catch(console.error);
+vegaEmbed('#scatter-index-talent', specVis1).then(function (result) { }).catch(console.error);
+vegaEmbed('#scatter-index-infrastructure', specVis2).then(function (result) { }).catch(console.error);
+vegaEmbed('#scatter-index-oe', specVis3).then(function (result) { }).catch(console.error);
+vegaEmbed('#scatter-index-research', specVis4).then(function (result) { }).catch(console.error);
+vegaEmbed('#scatter-index-development', specVis5).then(function (result) { }).catch(console.error);
+vegaEmbed('#scatter-index-commercial', specVis6).then(function (result) { }).catch(console.error);
+vegaEmbed('#scatter-index-gov', specVis7).then(function (result) { }).catch(console.error);
